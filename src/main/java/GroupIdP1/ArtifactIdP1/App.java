bfc7f76e770c18b0b4fdeeb04ecd42831d67a94e
LENOVO <LENOVO@LENOVO-PC>
2018-08-30 06:37:13 -0500

    update1

--- src/main/java/GroupIdP1/ArtifactIdP1/App.java
+++ src/main/java/GroupIdP1/ArtifactIdP1/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World1223123132" );
+        System.out.println( "Hello World1223123132" );
     }
 }
